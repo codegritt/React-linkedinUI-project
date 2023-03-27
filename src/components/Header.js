d26df6eb6e001c8836ffe835d900f9bcf036394e
gokul <gokulchandan18@gmail.com>
2023-03-27 10:33:03 +0530

    d

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
+      </div>
+
+      <div className="header__search">
+        <SearchIcon />
+        <input type="text" placeholder="Search" />
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
